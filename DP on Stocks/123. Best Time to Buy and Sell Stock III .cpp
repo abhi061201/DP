09936ff72b778b060901d5fdbcf93131dc10b19f
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-06-09 19:41:29 +0530

    Update 123. Best Time to Buy and Sell Stock III .cpp

--- DP on Stocks/123. Best Time to Buy and Sell Stock III .cpp
+++ DP on Stocks/123. Best Time to Buy and Sell Stock III .cpp
@@ -1,7 +1,10 @@
 /*
 TC---> O(n*2*3);
-SC---> O(n*2*3);
+SC---> O(n*2*3) + O(n)AST;
 */
+
+  // complete Transaction
+        // BUY----> SELL
 
 class Solution {
 public:
@@ -40,4 +43,57 @@ public:
         }
         return  dp[i][canbuy][transaction] =profit;
     }
+};
+
+/*
+TC---> O(n*2*3);
+SC---> O(n*2*3) ;
+*/
+
+class Solution {
+public:
+    vector<int>prices;
+    int maxProfit(vector<int>&Prices) {
+        prices=Prices;
+        int n= prices.size();
+        
+        int transaction=2;
+        // Since 3 parameters are changing therefore 3d dp
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        
+        // Base condition no need as already initialised with zero
+        dp[n][0][0]=0;
+        dp[n][0][1]=0;
+        dp[n][1][0]=0;
+        dp[n][1][1]=0;
+            
+            
+        for(int i=n-1 ; i>=0 ; i--){
+            for(int canbuy=0 ; canbuy<=1 ; canbuy++){
+               // transaction =0 no need as base case for every transaction=0 the value should be zero
+                for(transaction=1 ; transaction<=2 ; transaction++){
+                     
+                    int profit= 0;
+
+                    if(canbuy==1){
+                        profit= max(-prices[i]+ dp[i+1][0][transaction],dp[i+1][1][transaction]);
+                    }
+                    // on selling a stock  transaction complete therefore dectresed buy one . 
+                    else {
+
+                        // here in first case we sell stock, in second case we dosent sell stock therefore no decrement
+                        profit= max(prices[i]+ dp[i+1][1][transaction-1], dp[i+1][0][transaction]);
+                    }
+                     dp[i][canbuy][transaction] =profit;
+                }
+            }
+        }
+        
+        
+        return dp[0][1][2];
+        
+        
+    }
+    
+  
 };
