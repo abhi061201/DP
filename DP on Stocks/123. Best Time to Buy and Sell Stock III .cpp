c6a6b0379ce41d39c59d43719079d1555f9a41f6
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-06-09 19:53:06 +0530

    Update 123. Best Time to Buy and Sell Stock III .cpp

--- DP on Stocks/123. Best Time to Buy and Sell Stock III .cpp
+++ DP on Stocks/123. Best Time to Buy and Sell Stock III .cpp
@@ -97,3 +97,56 @@ public:
     
   
 };
+// Space optimised
+
+/*
+TC---> O(n*2*3);
+SC---> O(2*3) = constant;
+*/
+
+class Solution {
+public:
+    vector<int>prices;
+    int maxProfit(vector<int>&Prices) {
+        prices=Prices;
+        int n= prices.size();
+        
+        int transaction=2;
+        // Since 3 parameters are changing therefore 3d dp
+       vector<vector<int>>after(2,vector<int>(3,0));
+        vector<vector<int>>cur(2,vector<int>(3,0));
+        
+        
+      
+            
+            
+        for(int i=n-1 ; i>=0 ; i--){
+            for(int canbuy=0 ; canbuy<=1 ; canbuy++){
+                for(transaction=1 ; transaction<=2 ; transaction++){
+                     
+                    int profit= 0;
+
+                    if(canbuy==1){
+                        profit= max(-prices[i]+ after[0][transaction],after[1][transaction]);
+                    }
+                    // on selling a stock  transaction complete therefore dectresed buy one . 
+                    else {
+
+                        // here in first case we sell stock, in second case we dosent sell stock therefore no decrement
+                        profit= max(prices[i]+ after[1][transaction-1], after[0][transaction]);
+                    }
+                     cur[canbuy][transaction] =profit;
+                }
+            }
+            after=cur;
+        }
+        
+        
+        return after[1][2];
+        
+        
+    }
+    
+    // complete Transaction
+        // BUY----> SELL
+};
